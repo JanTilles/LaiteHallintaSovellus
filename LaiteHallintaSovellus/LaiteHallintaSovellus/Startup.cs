55443c6e3e6795cc57e5652bd34e5fecb06f3c98
Jan Tilles <jan.tilles@stud.pointcollege.fi>
2016-04-12 11:07:02 +0300

    Sivusto pohjat luotu

---
+++ LaiteHallintaSovellus/LaiteHallintaSovellus/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(LaiteHallintaSovellus.Startup))]
+namespace LaiteHallintaSovellus
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
